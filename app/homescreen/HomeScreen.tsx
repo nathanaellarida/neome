5a1d0a9d8737303d0ada28e565e47e770bb64b4a
laloweeee <osabellowe@gmail.com>
2025-04-15 17:41:27 +0800

    navigation on homescreen and messagehome

--- app/homescreen/HomeScreen.tsx
+++ app/homescreen/HomeScreen.tsx
@@ -522,7 +522,7 @@ export default function HomeScreen() {
           <Ionicons name="calendar-outline" size={25} color="#6549FE" />
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.navButton} onPress={() => router.push('/messaging/MessageHome')}>
+        <TouchableOpacity style={styles.navButton} onPress={() => router.push('./messaging/MessageHome')}>
           <Ionicons name="chatbubble-ellipses-outline" size={25} color="#6549FE" />
         </TouchableOpacity>
       </View>
