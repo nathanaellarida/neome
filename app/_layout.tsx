d2635c8f59b037cd65937a93758788da38cc019e
laloweeee <osabellowe@gmail.com>
2025-04-15 19:22:09 +0800

    merge main to own branch

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,7 @@
 import { Stack } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
-<<<<<<< HEAD
-import { FolderProvider } from './mental_activities/FolderContext';
-import { TaskProvider } from './mental_activities/TaskContext'; // ✅ ensure correct import
-=======
 import { FolderProvider } from '../contexts/FolderContext';
 import { TaskProvider } from '../contexts/TaskContext'; // ✅ moved out of app/
->>>>>>> main
 import { useEffect } from 'react';
 import { useRouter } from 'expo-router';
 
@@ -14,18 +9,6 @@ export default function Layout() {
   const router = useRouter();
 
   useEffect(() => {
-<<<<<<< HEAD
-    // Change this path to the screen you're working on
-    router.replace('./app/index');
-  }, []);
-
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-      </Stack>
-    </SafeAreaView>
-=======
     router.replace('/homescreen/HomeScreen');
   }, []);
 
@@ -40,6 +23,5 @@ export default function Layout() {
         </SafeAreaView>
       </TaskProvider>
     </FolderProvider>
->>>>>>> main
   );
 }
